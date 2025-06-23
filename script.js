9ed443bb5a0c994f95d9160c621c0712f5d509a4
Eduardo Roskoski <eduardo.roskoski.oliveira@escola.pr.gov.br>
2025-06-23 09:08:40 -0300

    index

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("loginForm").addEventListener("submit",function (e){
+e.preventDefault();
+
+const username = document.getElementById ("username").value;
+const password = document.getElementById ("password").value;
+const errorMessage = document.getElementById ("errorMessage");
+
+const validUsername = "admim"
+const validPassword = "123456"
+
+if(username === validUsername && password === validPassword)(
+    
+)
+
+}) 
